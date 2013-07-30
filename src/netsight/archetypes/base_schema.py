5273115225bbf4a4137b397a01cecbddc974bf06
Adam Cheasley <adam@netsight.co.uk>
2013-07-30 11:51:58 +0100

    add a base_folder schema. Hide creators schemata

--- src/netsight/archetypes/base_schema.py
+++ src/netsight/archetypes/base_schema.py
@@ -1,9 +1,16 @@
-from Products.Archetypes.BaseContent import BaseSchema
+from Products.ATContentTypes.content.schemata import ATContentTypeSchema
+from Products.ATContentTypes.content.folder import ATFolderSchema
 
 
-for field in BaseSchema.fields():
-    if field.schemata == 'metadata':
+absolute_base_schema = ATContentTypeSchema.copy()
+for field in absolute_base_schema.fields():
+    if field.schemata in ['metadata', 'creators']:
         field.widget.visible = False
 
 
-absolute_base_schema = BaseSchema.copy()
+absolute_base_folder_schema = ATFolderSchema.copy()
+for field in absolute_base_folder_schema.fields():
+    if field.schemata in ['metadata', 'creators']:
+        field.widget.visible = False
+    if field.getName() in ['location', 'language']:
+        field.widget.visible = False
