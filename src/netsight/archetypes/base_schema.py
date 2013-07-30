f27e5495832957cb2399241df58ccd83ff88a08f
Adam Cheasley <adam@netsight.co.uk>
2013-07-30 12:11:04 +0100

    hide location and language from absolute_base_schema

--- src/netsight/archetypes/base_schema.py
+++ src/netsight/archetypes/base_schema.py
@@ -6,6 +6,8 @@ absolute_base_schema = ATContentTypeSchema.copy()
 for field in absolute_base_schema.fields():
     if field.schemata in ['metadata', 'creators']:
         field.widget.visible = False
+    if field.getName() in ['location', 'language']:
+        field.widget.visible = False
 
 
 absolute_base_folder_schema = ATFolderSchema.copy()
